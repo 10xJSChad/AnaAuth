fc69b13fb7c31ff1218607e043163a06a47dad94
10xJSChad <48174610+10xJSChad@users.noreply.github.com>
2021-07-21 23:58:32 +0200

    Delete userClass.py

--- userClass.py
+++
@@ -1,6 +0,0 @@
-class user():
-    def __init__(self, username, password, ip, token):
-        self.username = username
-        self.password = password
-        self.ip = ip
-        self.token = token
